5735d89d10b06c87de57c9e6a305fe740e95b6cb
Katrusya Panchenko <katrusya.pan4enko@gmail.com>
2021-11-11 10:09:28 +0200

    решение первой задачи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+//обращение к div.gallery
+const galleryContainer =document.querySelector('.gallery')
+
+const galleryItemMarkup = creategalleryItemMarkup(galleryItems)
+//повесели слушателя событий на div.galler
+galleryContainer.addEventListener('click', onClickImg)
+//делегирования на div.gallery и получение url большого изображения
+function onClickImg(event) {
+ event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+   const openImg = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+
+openImg.show() 
+}
+// выход по кнопке esc
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    close();
+  }
+}
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup);
+//создание макета разметки галереи
+function creategalleryItemMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return` <div class = "gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+     
+    />
+  </a>
+</div>`;
+            
+        }).join('')
+  
+}
+
+console.log(galleryItemMarkup);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer =document.querySelector('.gallery')
+const galleryItemMarkup = creategalleryItemMarkup(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+function creategalleryItemMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return` <div class = "gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+    
+      alt="${description}"
+    />
+  </a>
+</div>`;
+            
+        }).join('')
+}
+ const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
